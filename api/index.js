fa4daf8dc6dc11a683c0f4142872dd63c8e4dc0f
Anthony Ng'ang'a <ngash.anto@gmail.com>
2023-12-05 19:14:13 +0300

    Connected the server to the database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running at port ${PORT}`)
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+
+    app.listen(PORT,()=>{
+        console.log(`Server is running at port ${PORT} and DB connected`)
+    })
 })
